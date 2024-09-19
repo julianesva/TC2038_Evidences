4f559d15236ab54ce323205fd6780073a943f0a0
Your Name <A01254679@tec.mx>
2024-09-18 23:41:31 -0600

    LCS added still working

--- main.cpp
+++ main.cpp
@@ -88,6 +88,41 @@ int KMPsearch(const std::string &transmissions, const std::string &virus) {
 
 
 
+
+
+    /*Este codigo enceuntra la subcadena comun mas larga entre dos cadenas.
+Utiliza una matriz(dp) para almacenar las longitudes de las subcadenas comunes
+entre s1 y s2. Si los caracteres coinciden, aumenta la longitud de la subcadena
+,si no, la reinica. La funcion retorna los indices donde comienza y termina la
+subcadena mas larga en s1.
+Time complexity: O(M*N)
+Space complexity: O(M*N)*/
+
+pair<int, int> LongestCommonSubstring(const string& s1, const string& s2) {
+    int n = s1.length();
+    int m = s2.length();
+    int dp[n + 1][m + 1];
+    int mx = 0, end = -1;
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; ++j) {
+            if (i == 0 || j == 0) {
+                dp[i][j] = 0;
+            } else if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+                if (mx < dp[i][j]) {
+                    mx = dp[i][j];
+                    end = i - 1;
+                }
+            } else {
+                dp[i][j] = 0;
+            }
+        }
+    }
+    return {end - mx + 1, end};
+}
+
+
 int main (){
 
     string virus1;
@@ -151,6 +186,13 @@ int main (){
             else{cout<< "FALSE : The transmission"<< i+1 << " don't contains the substring mcode" << j+1 << endl;}
         }
     }
+
+    // Perform the longest common substring analysis
+    auto [start, end] = LongestCommonSubstring(transmission1, transmission2);
+    cout << "\nLongest common substring between transmission1.txt and transmission2.txt:\n";
+    cout << "Start position in transmission1.txt: " << start << "\n";
+    cout << "End position in transmission1.txt: " << end << "\n";
+    cout << "Length: " << end - start + 1 << " characters\n";
 
     return 0; 
 }
